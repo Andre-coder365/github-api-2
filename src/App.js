f98b58aa861cc9a2ffd505b00f73180a79305cc0
Andre-coder365 <andreoliveira365@hotmail.com>
2022-06-28 19:38:54 -0300

    Adição do componente Profile e prorpiedades no styled-components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
 import Layout from "./components/layout";
+import Profile from "./components/profile";
 
 function App() {
   return (
     <main>
       <Layout>
-        <div>
-          <img src="https://avatars.githubusercontent.com/u/106115194?v=4" alt="Avatar of user"/>
-          <h1>André Oliveira</h1>
-          <h3>Username:</h3>
-          <span>Andre-coder365</span>
-          <div>
-            <div>
-            <h4>Followers</h4>
-            <span>5</span>
-            </div>
-            <div>
-            <h4>Starreds</h4>
-            <span>5</span>
-            </div>
-            <div>
-            <h4>Followings</h4>
-            <span>5</span>
-            </div>            
-          </div>
-        </div>
+        <Profile />
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
